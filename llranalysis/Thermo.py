Fixes to Free energy calculation
Fixes to Free energy calculation

llranalysis/Thermo.py
@@ -10,8 +10,17 @@ from scipy.interpolate import interp1d
 from scipy.interpolate import InterpolatedUnivariateSpline
 import matplotlib as mpl
 
+
 def find_critical_region(E,b):
-    cr = [len(InterpolatedUnivariateSpline(E,b - b[i]).roots())>=2 for i in range(len(b))]
+    dE = abs(E[1] - E[0])
+    #cr = [len(InterpolatedUnivariateSpline(E,b - b[i]).roots())>=2 for i in range(len(b))]
+    cr = []
+    #Changed to deal with a problem in a single ensemble :(
+    for i in range(len(b)):
+        roots = InterpolatedUnivariateSpline(E,b - b[i]).roots()
+        if (len(roots>=2)): 
+            cr.append((max(roots) - min(roots) > dE/10)) 
+        else: cr.append(False)
     mini = min(np.nonzero(cr)[0]); maxi= max(np.nonzero(cr)[0])
     midb = (b[mini] + b[maxi]) / 2. 
     spl = InterpolatedUnivariateSpline(E,b - midb).roots()

llranalysis/dElim.py
@@ -24,8 +24,10 @@ def compare_dE_plot_a(boot_folders, n_repeats, blim,ulim,num_samples=200, error_
             aks[i,:] = -final_df['a'].values
         aks_mean = aks.mean(axis = 0)
         aks_error = error.calculate_error_set(aks,num_samples,error_type)
-        ax1.errorbar(Eks/(6*V),aks_mean,aks_error, fmt = colours[j] + '-', label = '$a^4 \Delta_E / 6\\tilde{V}$' + f'= {2*dE / (6*V) :.4f}')
-        ax2.errorbar(Eks/(6*V),aks_mean,aks_error, fmt = colours[j] + '-', label = '$a^4 \Delta_E / 6\\tilde{V}$' + f'= {2*dE / (6*V) :.4f}')
+        #ax1.errorbar(Eks/(6*V),aks_mean,aks_error, fmt = colours[j] + '-', label = '$a^4 \Delta_E / 6\\tilde{V}$' + f'= {2*dE / (6*V) :.4f}')
+        #ax2.errorbar(Eks/(6*V),aks_mean,aks_error, fmt = colours[j] + '-', label = '$a^4 \Delta_E / 6\\tilde{V}$' + f'= {2*dE / (6*V) :.4f}')
+        ax1.errorbar(Eks/(6*V),aks_mean,aks_error, fmt = colours[j] + '-', label = '$\Delta_{u_p}$' + f'= {2*dE / (6*V) :.4f}')
+        ax2.errorbar(Eks/(6*V),aks_mean,aks_error, fmt = colours[j] + '-', label = '$\Delta_{u_p}$' + f'= {2*dE / (6*V) :.4f}')
         j += 1
     ax1.set_ylabel('$a_n$')
     ax2.set_ylabel('$a_n$')
@@ -93,7 +95,8 @@ def dE_DG_critical_beta(full_folders, reduced_folders, additional_folders, num_r
     print(f'All intervals all points & 0 & {DG_bc_0} ({DG_bc_0_err})')
     plt.errorbar(0,DG_bc_0,DG_bc_0_err, color=c, marker= '^', capsize=8, capthick=5, ms = markersize) 
     plt.ylabel('$\\beta_c$')
-    plt.xlabel('$(a^4 \\Delta_E / (6\\tilde{V}))^2$')
+    #plt.xlabel('$(a^4 \\Delta_E / (6\\tilde{V}))^2$')
+    plt.xlabel('$(\\Delta_{u_p})^2$')
     plt.ylim([miny,maxy])
     #plt.errorbar(np.NaN, np.NaN,np.NaN,marker = 'o', color = 'g',label = 'All intervals, all points')
     plt.errorbar(np.NaN, np.NaN,np.NaN,marker = 'o', color = 'darkorange',label = 'All intervals')
@@ -151,7 +154,8 @@ def dE_DG_critical_plaq(full_folders, reduced_folders, additional_folders, num_r
     print(f'All intervals all points & 0 & {up_bc_0} ({up_bc_0_err})')
     plt.errorbar(0,up_bc_0,up_bc_0_err, color=c, marker= '^', capsize=8, capthick=5, ms = markersize)  
     plt.ylabel('$\Delta \\langle u_p \\rangle_{\\beta_c}$')
-    plt.xlabel('$(a^4 \\Delta_E / (6\\tilde{V}))^2$')
+    #plt.xlabel('$(a^4 \\Delta_E / (6\\tilde{V}))^2$')
+    plt.xlabel('$(\\Delta_{u_p})^2$')
     #plt.errorbar(np.NaN, np.NaN,np.NaN,marker = 'o', color = 'g',label = 'All intervals, all points')
     plt.errorbar(np.NaN, np.NaN,np.NaN,marker = 'o', color = 'darkorange',label = 'All intervals')
     plt.errorbar(np.NaN, np.NaN,np.NaN,marker= 'o',color= 'b', label = 'Odd intervals')
@@ -200,7 +204,8 @@ def compare_dE_plot_y(boot_folders, n_repeats, std_files, std_folder, std_key, l
             plt.axvspan(bmax_avr - bmax_err,bmax_avr + bmax_err, alpha=0.2, color=colours[i])
             plt.axvline(bmax_avr, color=colours[i],linestyle='dashed')
         llr_full_y = llr_full_y.mean(axis=0)
-        plt.plot(llr_full_b, llr_full_y, colours[i] + '-', label = 'LLR  $a^4 \Delta_E/ 6\\tilde{V}$' + f'= {2*dE / (6*V):.4f}' ,zorder=i) #
+        #plt.plot(llr_full_b, llr_full_y, colours[i] + '-', label = 'LLR  $a^4 \Delta_E/ 6\\tilde{V}$' + f'= {2*dE / (6*V):.4f}' ,zorder=i) #
+        plt.plot(llr_full_b, llr_full_y, colours[i] + '-', label = 'LLR  $\Delta_{u_p}$' + f'= {2*dE / (6*V):.4f}' ,zorder=i) #
         plt.plot(llr_full_b, llr_full_y +llr_full_y_err, colours[i] + '--')
         plt.plot(llr_full_b,llr_full_y - llr_full_y_err, colours[i] + '--')
         plt.errorbar(llr_comp_b, llr_comp_y,llr_comp_y_err, fmt = colours[i] + 'o', capsize=10)
@@ -238,7 +243,8 @@ def compare_dE_plot_y_difference(boot_folders, n_repeats, std_files, std_folder,
         b = std_ys; db = std_ys_err;
         c = (a - b)/ b;
         dc = (a/b) * ((((da/a)**2) + ((db/b)**2))**0.5)
-        plt.errorbar(llr_comp_b, c,dc, fmt = colours[i] + 'o', capsize=10, label = 'LLR  $a^4 \Delta_E/ 6\\tilde{V}$' + f'= {2*dE / (6*V):.4f}',zorder=i)
+        #plt.errorbar(llr_comp_b, c,dc, fmt = colours[i] + 'o', capsize=10, label = 'LLR  $a^4 \Delta_E/ 6\\tilde{V}$' + f'= {2*dE / (6*V):.4f}',zorder=i)
+        plt.errorbar(llr_comp_b, c,dc, fmt = colours[i] + 'o', capsize=10, label = 'LLR  $\Delta_{u_p}$' + f'= {2*dE / (6*V):.4f}',zorder=i)
         print(llr_comp_b, dE/(3*V))
         print(c,dc)
     #plt.errorbar(std_bs, 0.*std_ys, std_ys_err, fmt ='k^', label='Importance sampling', capsize=10)
@@ -261,6 +267,8 @@ def compare_dE_plot_ymax(boot_folders, n_repeats, llr_key, label,minimum=False,n
         full_b = np.array([])
         for j in range(nr):
             full_df = pd.read_csv(f'{bf}{j}/CSV/obs_critical.csv')
+            if(full_df[llr_key].values.sum() == 0):
+                full_df = pd.read_csv(f'{bf}{j}/CSV/obs.csv')
             y = [full_df.iloc[i][llr_key] for i in range(len(full_df)) if not np.isnan(full_df.iloc[i][llr_key])]
             b = [full_df.iloc[i]['b'] for i in range(len(full_df)) if not np.isnan(full_df.iloc[i][llr_key])]
             if minimum: arg = np.argmin(y)
@@ -279,7 +287,9 @@ def compare_dE_plot_ymax(boot_folders, n_repeats, llr_key, label,minimum=False,n
     print(ymax)
     print(ymax_err)
     ylim, ylim_err = utils.plot_extrap(dEsq,ymax,ymax_err, np.arange(len(dEsq)), 'k')
-    plt.xlabel('$(a^4 \Delta_E/ 6\\tilde{V})^2$', fontsize = 30)
+    print(ylim, ylim_err)
+    #plt.xlabel('$(a^4 \Delta_E/ 6\\tilde{V})^2$', fontsize = 30)
+    plt.xlabel('$(\Delta_{u_p})^2$', fontsize = 30)
     plt.locator_params(axis="x", nbins=7)
     plt.show()    
 

llranalysis/llr.py
@@ -469,6 +469,8 @@ def half_intervals(originalfolder, reducedfolder, mode='even'):
 def free_energy_df(boot_folder,n_repeats,num_samples,error_type):
     final_df = pd.read_csv(f'{boot_folder}/CSV/final.csv')
     V = final_df['V'][0]
+    info_df = pd.read_csv(f'{boot_folder}/CSV/info.csv')
+    Lt = info_df['Lt'][0]; Ls = info_df['Ls'][0];
     Ep = np.unique(final_df['Ek'])
     S,T,F,U = thermo.thermodynamics(boot_folder,n_repeats, Ep)
     Sigma = S.mean()
@@ -497,16 +499,26 @@ def free_energy_df(boot_folder,n_repeats,num_samples,error_type):
     P_max_F= np.array([])
     up_min =  np.array([])
     up_max = np.array([])
+    sigma = np.array([])
 
     xs = np.array([]);ys = np.array([])
     for i in range(n_repeats):
-        f = interp1d(T_int[i,meta_h],F_int[i, meta_h])
-        g = interp1d(T_int[i,meta_c],F_int[i, meta_c])
-        t = np.linspace(max(min(T_int[i,meta_h]),min(T_int[i,meta_c])),min(max(T_int[i,meta_h]),max(T_int[i,meta_c])), 1000)
-        tc_ind = np.where(T_int[i,:] == T_int[i,np.append(meta_h, meta_c)][np.argmin(abs(T_int[i,np.append(meta_h, meta_c)] - t[np.argmin(abs(f(t)-g(t)))]))])[0]
+        try:
+            f = interp1d(T_int[i,meta_h],F_int[i, meta_h])
+            g = interp1d(T_int[i,meta_c],F_int[i, meta_c])
+            t = np.linspace(max(min(T_int[i,meta_h]),min(T_int[i,meta_c])),min(max(T_int[i,meta_h]),max(T_int[i,meta_c])), 10000)
+            tc_ind = np.where(T_int[i,:] == T_int[i,np.append(meta_h, meta_c)][np.argmin(abs(T_int[i,np.append(meta_h, meta_c)] - t[np.argmin(abs(f(t)-g(t)))]))])[0]
+        except:
+            print('Error no overlap in two meta-stable branches, likely misclassified : including unstable branch')
+            mc = meta_c; mh = np.append(meta_h,unstable).flatten()
+            f = interp1d(T_int[i,mh],F_int[i,mh])
+            g = interp1d(T_int[i,mc],F_int[i, mc])
+            t = np.linspace(max(min(T_int[i,mh]),min(T_int[i,mc])),min(max(T_int[i, mh]),max(T_int[i,mc])),  10000)
+            tc_ind = np.where(T_int[i,:] == T_int[i,np.append(mh, mc)][np.argmin(abs(T_int[i,np.append(mh,mc)] - t[np.argmin(abs(f(t)-g(t)))]))])[0]
         tmin = T_int[i,tc_ind]
         pminf = F_int[i,tc_ind]
-        P_min_F = np.append(P_min_F,F_int[i,unstable][np.argmin(abs(T_int[i,unstable] - tmin))])
+        pmaxf = F_int[i,unstable][np.argmin(abs(T_int[i,unstable] - tmin))]
+        P_min_F = np.append(P_min_F,pmaxf)
         P_max_F = np.append(P_max_F ,pminf)
         Tc = np.append(Tc,tmin)
         
@@ -518,14 +530,15 @@ def free_energy_df(boot_folder,n_repeats,num_samples,error_type):
         lnz = float(calc_lnZ(final_df['Ek'].values, final_df['a'].values, beta))
         x, y = calc_prob_distribution(final_df, beta, lnz)
         xs = np.append(xs, x); ys = np.append(ys, y * (6*V))
-    xs.shape = [n_repeats, len(x)]; ys.shape = [n_repeats, len(y)]
 
+    xs.shape = [n_repeats, len(x)]; ys.shape = [n_repeats, len(y)]
 
     F_int -= P_max_F.mean()
     F -= P_max_F.mean()
     P_min_F -= P_max_F.mean()
     P_max_F -= P_max_F.mean()
     
+    sigma =  ( ( (Lt**3) * Ls * (P_min_F - P_max_F) ) / (2*Tc)) + (((Lt**2)*np.log(Ls) ) / (4*(Ls**2)) )
     
     for i in range(n_repeats):
         pd.DataFrame(data = {'Tc':Tc[i],'Fcmin':P_min_F[i],'Fcmax':P_max_F[i],
@@ -533,7 +546,7 @@ def free_energy_df(boot_folder,n_repeats,num_samples,error_type):
                                     'F_int':[list(F_int[i,:])], 'T_int':[list(T_int[i,:])], 'S_int':[list(S_int[i,:])],'U_int':[list(U_int[i,:])],
                                     'up-':up_min[i] ,'up+':up_max[i],
                                     'ind_c': [[mini,meta_mini, midi,meta_maxi, maxi]], 
-                                    'E_Pb':[list(xs[i,:])], 'Pb':[list(ys[i,:])] }).to_csv(f'{boot_folder}{i}/CSV/F.csv')
+                                    'E_Pb':[list(xs[i,:])], 'Pb':[list(ys[i,:])]}).to_csv(f'{boot_folder}{i}/CSV/F.csv')
     
     F_err = error.calculate_error_set(F,num_samples,error_type);
     T_err = error.calculate_error_set(T,num_samples,error_type);
@@ -546,11 +559,10 @@ def free_energy_df(boot_folder,n_repeats,num_samples,error_type):
     
     Tc_err = error.calculate_error(Tc,num_samples,error_type); Tc = Tc.mean(axis=0);
     dF_err = error.calculate_error(P_min_F - P_max_F,num_samples,error_type); dF = (P_min_F - P_max_F).mean(axis=0);
-    
     du_err = error.calculate_error(up_max -up_min,num_samples,error_type);du = (up_max -up_min).mean(axis=0);
     up_min_err = error.calculate_error(up_min,num_samples,error_type); up_min = up_min.mean(axis=0);
     up_max_err = error.calculate_error(up_max,num_samples,error_type); up_max = up_max.mean(axis=0);
-    
+    sigma_err = error.calculate_error(sigma,num_samples,error_type);sigma = sigma.mean();
     xs = xs.mean(axis = 0)
     ys_err = error.calculate_error_set(ys, num_samples, error_type)
     ys = ys.mean(axis = 0)
@@ -563,7 +575,8 @@ def free_energy_df(boot_folder,n_repeats,num_samples,error_type):
                         'up-':up_min ,'up-_err':up_min_err ,'up+':up_max,'up+_err':up_max_err,
                          'du':du ,'du_err':du_err ,
                         'ind_c': [[mini,meta_mini, midi,meta_maxi, maxi]],
-                        'E_Pb':[list(xs)], 'Pb':[list(ys)], 'Pb_err':[list(ys_err)]}).to_csv(f'{boot_folder}CSV/F.csv')  
+                        'E_Pb':[list(xs)], 'Pb':[list(ys)], 'Pb_err':[list(ys_err)],
+                        'Sigma':sigma,'Sigma_err':sigma_err}).to_csv(f'{boot_folder}CSV/F.csv')  
 
 def pre_dat(folder,V,up_min,up_max,N_intervals, betas, location):
     is_df = pd.read_csv(f'{folder}std.csv')
@@ -680,16 +693,16 @@ def obs_boot(folder, n_repeat, num_samples, error_type):
     Cuc_err = error.calculate_error(Cuc, num_samples, error_type);
     Bvc_err = error.calculate_error(Bvc, num_samples, error_type);
     Xlpc_err = error.calculate_error(Xlpc, num_samples, error_type);
-    
+    pd.DataFrame(data = {'b_Xlp':bc_Xlp,'b_Xlp_err':bc_Xlp_err,'Xlp':Xlpc, 'Xlp_err':Xlpc_err,
+                         'b_Cu':bc_Cu,'b_Cu_err':bc_Cu_err,'Cu':Cuc, 'Cu_err':Cuc_err,
+                         'b_Bv':bc_Bv,'b_Bv_err':bc_Bv_err,'Bv':Bvc, 'Bv_err':Bvc_err}, index=[0]).to_csv(folder + 'CSV/critical.csv')
     bc_Xlp = bc_Xlp.mean();
     bc_Cu = bc_Cu.mean();
     bc_Bv = bc_Bv.mean();
     Cuc = Cuc.mean();
     Bvc = Bvc.mean();
     Xlpc = Xlpc.mean();
-    pd.DataFrame(data = {'b_Xlp':bc_Xlp,'b_Xlp_err':bc_Xlp_err,'Xlp':Xlpc, 'Xlp_err':Xlpc_err,
-                         'b_Cu':bc_Cu,'b_Cu_err':bc_Cu_err,'Cu':Cuc, 'Cu_err':Cuc_err,
-                         'b_Bv':bc_Bv,'b_Bv_err':bc_Bv_err,'Bv':Bvc, 'Bv_err':Bvc_err}, index=[0]).to_csv(folder + 'CSV/critical.csv')
+
     
 def final_boot(folder, n_repeat, num_samples, error_type):
     ak = np.array([]);Ek = np.array([]); 
@@ -711,7 +724,7 @@ def DG_boot(folder, n_repeat, num_samples, error_type):
     for j in range(n_repeat):
         df = pd.read_csv(f'{folder}{j}/CSV/DG.csv')
         bc = np.append(bc, df['Bc']);
-        dup = np.append(dup, df['Bc'] / (6*df['V']));
+        dup = np.append(dup, df['lh'] / (6*df['V']));
         dP =  np.append(dP, df['dP']);
     bc_err = error.calculate_error(bc, num_samples, error_type); 
     dup_err = error.calculate_error(dup, num_samples, error_type); 